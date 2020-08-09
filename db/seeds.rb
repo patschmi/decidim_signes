fbc8451db5f439e87ff13e6e075baea56adcb9b9
patschmi <patschmi78@gmail.com>
2020-08-09 13:18:14 +0200

    pas de seed faker juste superadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,37 +11,41 @@ if ENV["HEROKU_APP_NAME"].present?
   ENV["SEED"] = "true"
 end
 
-require "decidim/faker/localized"
+# require "decidim/faker/localized"
 
-Decidim::Organization.first || Decidim::Organization.create!(
-  name: Faker::Company.name,
-  twitter_handler: Faker::Hipster.word,
-  facebook_handler: Faker::Hipster.word,
-  instagram_handler: Faker::Hipster.word,
-  youtube_handler: Faker::Hipster.word,
-  github_handler: Faker::Hipster.word,
-  smtp_settings: {
-    from: ENV["EMAIL"],
-    user_name: ENV["SENDGRID_USERNAME"],
-    encrypted_password: Decidim::AttributeEncryptor.encrypt(ENV["SENDGRID_PASSWORD"]),
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  },
-  host: ENV["DECIDIM_HOST"] || "localhost",
-  description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
-    Decidim::Faker::Localized.sentence(15)
-  end,
-  default_locale: Decidim.default_locale,
-  available_locales: Decidim.available_locales,
-  reference_prefix: Faker::Name.suffix,
-  available_authorizations: Decidim.authorization_workflows.map(&:name),
-  users_registration_mode: :enabled,
-  tos_version: Time.current,
-  badges_enabled: true,
-  user_groups_enabled: true,
-  send_welcome_notification: true
-)
+puts "create user"
+user = Decidim::System::Admin.new(email: "nicolas@gmail.com", password: "nounours", password_confirmation: "nounours")
+user.save!
 
-Decidim.seed!
+# Decidim::Organization.first || Decidim::Organization.create!(
+#   name: Faker::Company.name,
+#   twitter_handler: Faker::Hipster.word,
+#   facebook_handler: Faker::Hipster.word,
+#   instagram_handler: Faker::Hipster.word,
+#   youtube_handler: Faker::Hipster.word,
+#   github_handler: Faker::Hipster.word,
+#   smtp_settings: {
+#     from: ENV["EMAIL"],
+#     user_name: ENV["SENDGRID_USERNAME"],
+#     encrypted_password: Decidim::AttributeEncryptor.encrypt(ENV["SENDGRID_PASSWORD"]),
+#     address: "smtp.sendgrid.net",
+#     port: 587,
+#     authentication: :plain,
+#     enable_starttls_auto: true
+#   },
+#   host: ENV["DECIDIM_HOST"] || "localhost",
+#   description: Decidim::Faker::Localized.wrapped("<p>", "</p>") do
+#     Decidim::Faker::Localized.sentence(15)
+#   end,
+#   default_locale: Decidim.default_locale,
+#   available_locales: Decidim.available_locales,
+#   reference_prefix: Faker::Name.suffix,
+#   available_authorizations: Decidim.authorization_workflows.map(&:name),
+#   users_registration_mode: :enabled,
+#   tos_version: Time.current,
+#   badges_enabled: true,
+#   user_groups_enabled: true,
+#   send_welcome_notification: true
+# )
+
+# Decidim.seed!
